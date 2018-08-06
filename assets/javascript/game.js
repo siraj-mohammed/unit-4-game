7060dc159fdfcf627dd0db5b5d1def61d68210ce
MaximusSirilius <40532864+siraj-mohammed@users.noreply.github.com>
2018-08-05 22:04:50 -0400

    Styling updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,22 @@ var imageCrystal;
 var wins = 0;
 var losses = 0;
 
+// Begin Modal elements and functionality 
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("btnInst");
+var span = document.getElementsByClassName("close")[0]; 
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+$(document).on("click", ".modal", function(){
+    this.style.display = "none";
+});
+// End Modal elements and functionality
+
 function generateRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -34,14 +50,14 @@ function startGame(){
     $("#random-number").text(targetNumber);
     $("#wins").text(" " + wins);
     $("#losses").text(" " + losses);
-    $("#score-2").text(score);
+    $("#score").text(score);
 }
 
 $(document).on("click", ".crystal-image", function(){
     crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     score += crystalValue;
-    $("#score-2").text(score);
+    $("#score").text(score);
     if (score === targetNumber) {
         wins++;
         $("#result").text(" You win!");
@@ -54,4 +70,4 @@ $(document).on("click", ".crystal-image", function(){
     }
   });
 
-  startGame();
+$(document).on("ready", startGame());
